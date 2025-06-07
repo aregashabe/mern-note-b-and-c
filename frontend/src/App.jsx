0b207f416082497af7e0ff48eee9f3b36cd053dc
aregashabe <areguaabebe@gmail.com>
2025-06-07 03:14:53 +0300

    change vercel on frontend and route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react"
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom"
-import Home from "./pages/Home/Home"
-import Login from "./pages/Login/Login"
-import Signup from "./pages/Signup/Signup"
-import { ToastContainer } from "react-toastify"
-import "react-toastify/dist/ReactToastify.css"
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
@@ -13,10 +13,21 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        {/* Add a catch-all route */}
+        <Route path="*" element={<Home />} />
       </Routes>
-      <ToastContainer position="top-center" />
+      <ToastContainer 
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar
+        newestOnTop
+        closeOnClick
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
