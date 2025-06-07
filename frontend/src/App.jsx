6ceaba01083a491b8e945714d279de5e2940d6fe
aregashabe <areguaabebe@gmail.com>
2025-06-07 04:05:12 +0300

    versel backend change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,15 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Login from "./pages/Login/Login";
-import Signup from "./pages/Signup/Signup";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-const App = () => {
+function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        {/* Add a catch-all route */}
-        <Route path="*" element={<Home />} />
+        {/* Catch-all route */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-      <ToastContainer 
-        position="top-center"
-        autoClose={3000}
-        hideProgressBar
-        newestOnTop
-        closeOnClick
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
     </BrowserRouter>
   );
-};
-
-export default App;
+}
